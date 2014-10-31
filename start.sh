b634b430ec39b1ae608868e4ff208608a27fdc56
Naoki AINOYA <ainonic@gmail.com>
2014-10-31 11:18:01 +0900

    make supervisord subprocess

--- start.sh
+++ start.sh
@@ -43,7 +43,7 @@ if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
 fi
 
 # start all the services
-/usr/local/bin/supervisord -n
+/usr/local/bin/supervisord -n &
 
 # stop myself after 5 minutes
 sleep 300 && pkill supervisord
